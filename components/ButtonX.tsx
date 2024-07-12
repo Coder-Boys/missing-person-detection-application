4495b260b8e67bb2c563806ff1e451a19f635939
Prantor-Hawlader <prantor502@gmail.comm>
2024-07-12 11:45:55 +0600

    delete btn updated

--- components/ButtonX.tsx
+++ components/ButtonX.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 const ButtonX = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+  const handleDelete = () => {
+    setTimeout(() => {
+      router.refresh();
+    }, 1000);
+  };
   return (
-    <Button type="submit" className="w-full">
+    <Button variant="destructive" onClick={handleDelete}>
       {children}
     </Button>
   );
